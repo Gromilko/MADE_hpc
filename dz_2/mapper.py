c21c3efb64f7d8c67f88aca8847a5ca104d335a2
Futura <zloy_dyadka@mail.ru>
2020-10-03 14:33:31 +0300

    dz_1

--- dz_2/mapper.py
+++
@@ -1,9 +0,0 @@
-"""mapper.py"""
-import sys
-
-for line in sys.stdin:
-    if line.startswith('id'):
-        continue
-    line = line.strip()
-    line = line.split('\t')
-    print(f'{1}\t{line[9]}')
